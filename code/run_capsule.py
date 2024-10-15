bd6c708f3d2530343427dce824937363b2c23b19
Camilo Laiton <camilo.laiton@alleninstitute.org>
2024-10-15 20:11:28 +0000

    Converting structure to pipeline like structure

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -24,56 +24,59 @@ def run():
     utils.create_folder(dest_dir=str(output_folder), verbose=True)
 
     # Data
-    IMAGE_PATH = "HCR_736207-01_2024-08-08_13-00-00/fused/channel_638.zarr"
+    data_channels = DATA_FOLDER.glob("channel*.zarr")
+    segmentation_paths = DATA_FOLDER.glob("segmentation_*.zarr")
+    
+    if len(data_channels) and len(segmentation_paths):
+        data_path = data_channels[0]
+        segmentation_path = segmentation_paths[0]
 
-    DATA_PATH = f"{DATA_FOLDER}/{IMAGE_PATH}"
-    SEGMENTATION_PATH = f"{DATA_FOLDER}/HCR_736207-01_2024-08-08_13-00-00_upsampled-masks/segmentation_mask.zarr"
-    # If using the bucket path directly, provide credentials to the capsule
-    # f"s3://{BUCKET_NAME}/{IMAGE_PATH}"
+        logger = utils.create_logger(output_log_path=str(output_folder))
 
-    logger = utils.create_logger(output_log_path=str(output_folder))
+        logger.info(f"Processing dataset {data_path} with segmentation {segmentation_path}")
+        # Puncta detection parameters
 
-    # Puncta detection parameters
+        sigma_zyx = [1.8, 1.0, 1.0]
+        background_percentage = 25
+        axis_pad = int(1.6 * max(max(sigma_zyx[1:]), sigma_zyx[0]) * 5)
+        min_zyx = [3, 3, 3]
+        filt_thresh = 20
+        raw_thresh = 180
+        context_radius = 3
+        radius_confidence = 0.05
 
-    sigma_zyx = [1.8, 1.0, 1.0]
-    background_percentage = 25
-    axis_pad = int(1.6 * max(max(sigma_zyx[1:]), sigma_zyx[0]) * 5)
-    min_zyx = [3, 3, 3]
-    filt_thresh = 20
-    raw_thresh = 180
-    context_radius = 3
-    radius_confidence = 0.05
+        # Data loader params
+        puncta_params = {
+            "dataset_path": data_path,
+            "segmentation_mask_path": segmentation_path,
+            "multiscale": "0",
+            "prediction_chunksize": (128, 128, 128),
+            "target_size_mb": 3048,
+            "n_workers": 0,
+            "batch_size": 1,
+            "axis_pad": axis_pad,
+            "output_folder": output_folder,
+            "logger": logger,
+            "super_chunksize": None,
+            "spot_parameters": {
+                "sigma_zyx": sigma_zyx,
+                "background_percentage": background_percentage,
+                "min_zyx": min_zyx,
+                "filt_thresh": filt_thresh,
+                "raw_thresh": raw_thresh,
+                "context_radius": context_radius,
+                "radius_confidence": radius_confidence,
+            },
+        }
 
-    # Data loader params
-    puncta_params = {
-        "dataset_path": DATA_PATH,
-        "segmentation_mask_path": SEGMENTATION_PATH,
-        "multiscale": "0",
-        "prediction_chunksize": (128, 128, 128),
-        "target_size_mb": 3048,
-        "n_workers": 0,
-        "batch_size": 1,
-        "axis_pad": axis_pad,
-        "output_folder": output_folder,
-        "logger": logger,
-        "super_chunksize": None,
-        "spot_parameters": {
-            "sigma_zyx": sigma_zyx,
-            "background_percentage": background_percentage,
-            "min_zyx": min_zyx,
-            "filt_thresh": filt_thresh,
-            "raw_thresh": raw_thresh,
-            "context_radius": context_radius,
-            "radius_confidence": radius_confidence,
-        },
-    }
+        logger.info(
+            f"Dataset path: {puncta_params['dataset_path']} - Puncta detection params: {puncta_params}"
+        )
 
-    logger.info(
-        f"Dataset path: {puncta_params['dataset_path']} - Puncta detection params: {puncta_params}"
-    )
+        z1_puncta_detection(**puncta_params)
 
-    z1_puncta_detection(**puncta_params)
-
+    else:
+        raise FileNotFoundError("There are no image channels or segmentation data inside of the data folder.")
 
 if __name__ == "__main__":
     # cProfile.run('main()', filename="/results/compute_costs.dat")
